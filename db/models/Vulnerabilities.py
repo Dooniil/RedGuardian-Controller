ab794b488f28d7fd468168701495ee12ff95e172
Dooniil2 <dooniil2@yandex.ru>
2023-02-03 17:35:59 +0300

    little changes

--- db/models/Vulnerabilities.py
+++ db/models/Vulnerabilities.py
@@ -1,14 +1,22 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 
 from db.config import Base
 
 
-# class Family(Base):
-#     __tablename__ = 'family'
-#
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String, nullable=False)
-#
+class Family(Base):
+    __tablename__ = 'family'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+
+class Platform(Base):
+    __tablename__ = 'platform'
+
+    id = Column(Integer, primary_key=True)
+    family_id = Column(Integer, ForeignKey('family.id'), nullable=False)
+    name = Column(String, nullable=False)
+
 
 class Vulnerability(Base):
     __tablename__ = 'vulnerability'
@@ -16,7 +24,8 @@ class Vulnerability(Base):
     id = Column(Integer, primary_key=True)
     cve_id = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    # family_id = Column(Integer, ForeignKey('family.id'), nullable=False)
-    # severity = Column(Integer, nullable=False)
-    cmd = Column(String, nullable=False)
-    cmd_res = Column(String, nullable=False)
+    severity = Column(Integer, nullable=False)
+    cmd_86 = Column(JSON, nullable=False)
+    cmd_64 = Column(JSON, nullable=True)
+    platform_id = Column(Integer, ForeignKey('platform.id'), nullable=False)
+    versions = Column(JSON, nullable=True)
